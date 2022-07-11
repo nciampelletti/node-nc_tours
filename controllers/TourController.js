851a298dfe3a88380de859a6c697e3be8046a03b
nciampelletti <natashat@shaw.ca>
2022-07-11 11:26:46 -0700

    refactoring API features

--- controllers/TourController.js
+++ controllers/TourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../model/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 //ROUTE HANDLERS
 exports.aliasTopTours = async (req, res, next) => {
@@ -12,70 +13,14 @@ exports.aliasTopTours = async (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    /* BUILD QUERY */
-    //case 1
-    //1a. Filtering
-    const queryObj = { ...req.query };
-    const excludeField = ['page', 'sort', 'limit', 'fields'];
-
-    excludeField.forEach((el) => delete queryObj[el]);
-
-    //1b. Advance filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //2. Sorting
-    if (req.query.sort) {
-      //http://localhost:8000/api/v1/tours?sort=-price,ratingAverage
-      const sortBy = req.query.sort.split(',').join(' ');
-
-      query = query.sort(sortBy);
-    } else {
-      //default sort
-      query = query.sort('-createdAt');
-    }
-
-    //3. Fields limiting
-    //http://localhost:8000/api/v1/tours?fields=name,duration,difficulty,price
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-
-      query = query.select(fields);
-    } else {
-      //default
-      query = query.select('-__v'); //exclude last field
-    }
-
-    //4. Pagination
-    //http://localhost:8000/api/v1/tours?page=2&limit=10
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skipValue = (page - 1) * limit;
-
-    query = query.skip(skipValue).limit(limit);
-    if (req.query.page) {
-      const numTours = await Tours.countDocuments();
-      if (skipValue >= numTours) throw new Error('This page doesnt exist');
-    }
-
     /* EXECUTE QUERY */
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    ////case 2
-    // const query =  Tour.find({
-    //   duration: req.query.duration,
-    //   difficulty: req.query.difficulty,
-    // });
-
-    ////case 3
-    // const query =  Tour.find()
-    //   .where('duration')
-    //   .equals(req.query.duration)
-    //   .where('difficulty')
-    //   .equals(req.query.difficulty);
+    const tours = await features.query;
 
     /* SEND RESPONSE */
     res.status(200).json({
---
+++ utils/apiFeatures.js
@@ -0,0 +1,62 @@
+class APIFeatures {
+  //query - mongoose query, queryString - query that comes from route
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  /* FILTERING */
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludeField = ['page', 'sort', 'limit', 'fields'];
+
+    excludeField.forEach((el) => delete queryObj[el]);
+
+    //1b. Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      //http://localhost:8000/api/v1/tours?sort=-price,ratingAverage
+      const sortBy = this.queryString.sort.split(',').join(' ');
+
+      this.query = this.query.sort(sortBy);
+    } else {
+      //default sort
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+
+      this.query = this.query.select(fields);
+    } else {
+      //default
+      this.query = this.query.select('-__v'); //exclude last field
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skipValue = (page - 1) * limit;
+
+    this.query = this.query.skip(skipValue).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
