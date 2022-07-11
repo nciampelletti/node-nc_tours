453a68a48976665db474a9f1a84f29cc1ee80420
nciampelletti <natashat@shaw.ca>
2022-07-11 12:04:14 -0700

    implement aggregation pipeling for matching and grouping

--- routes/TourRoutes.js
+++ routes/TourRoutes.js
@@ -8,14 +8,15 @@ const {
   checkID,
   checkBody,
   aliasTopTours,
+  getToursStats,
 } = require('../controllers/TourController');
 
 const router = express.Router();
 
-//middlewear function for param id
-// router.param('id', checkID);
-
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getToursStats);
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
